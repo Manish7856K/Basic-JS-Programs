e489f6b7d006a5815facad49270964aa46de30a7
First Commit Text File <manishkasoju2005@gmail.com>
2026-05-04 14:36:38 +0530

    Factorial

--- switch.js
+++ switch.js
@@ -3,9 +3,14 @@ let num2=20;
 let operator="+"
 switch(operator){
     case "+":console.log(num1+num2);
+                    break;
     case "-":console.log(num1-num2);
+                    break;
     case "*":console.log(num1*num2);
+                    break;
     case "/":console.log(num1/num2);
+                    break;
     case "%":console.log(num1%num2);
+                    break;
 
 }
